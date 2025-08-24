9b0f754b545bf4fe73e9777226367691f6925fbf
r9-9950x <1281261856@qq.com>
2025-08-24 16:45:56 +0800

    update

--- libhb/cpy.sh
+++ libhb/cpy.sh
@@ -6,8 +6,8 @@ CURRENT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd ${CURRENT_DIR}
 
 libhb_dir="H:/dev_hb/libhb"
-src_dir=${libhb_dir}/
-dst_dir=${CURRENT_DIR}/
+src_dir=${libhb_dir}
+dst_dir=${CURRENT_DIR}
 files="
 libemb/time/gmtime.c
 libemb/time/localtime.c
@@ -19,7 +19,7 @@ libemb/emb_time.h
 libstc/c51io/c51io.c
 libstc/c51io/c51io.sh
 libstc/stc_helper_gpio.h
-libstc/stc_helper.h
+libstc/stc_helpers.h
 libstc/stc8h.c
 libstc/stc8h.h
 "
@@ -27,3 +27,10 @@ libstc/stc8h.h
 for file in ${files}; do
     cp ${src_dir}/${file} ${dst_dir}/${file}
 done
+
+# patch libstc/c51io/c51io.sh
+# replace `CC="/e/local_program/tcc-0.9.27-win64-bin/tcc/tcc.exe"` with `CC="${CURRENT_DIR}/../../../tcc-0.9.27-win64-bin/tcc/tcc.exe"`
+sed -i 's|CC="/e/local_program/tcc-0.9.27-win64-bin/tcc/tcc.exe"|CC="${CURRENT_DIR}/../../../tcc-0.9.27-win64-bin/tcc/tcc.exe"|' ${dst_dir}/libstc/c51io/c51io.sh
+# replace `-I"H:/dev_hb/libhb" \` with `-I"../libhb" \`
+sed -i 's|-I"H:/dev_hb/libhb" |-I"../libhb" |' ${dst_dir}/libstc/c51io/c51io.sh
+
