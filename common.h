bb547b0dce3ccdd3a4b619523ec2f0c2edb4b9b9
r9-9950x <1281261856@qq.com>
2025-08-28 15:51:11 +0800

    update

--- common.h
+++ common.h
@@ -1,3 +1,8 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+//////////////////////////// change this by specific chip ////////////////////////////
+
 /**
  * chip: STC8H8K64U
  * ram: 256B idata, 8KB xdata
@@ -6,32 +11,40 @@
  *   - 60KB for application
  */
 
-#ifndef __COMMON_H__
-#define __COMMON_H__
-
-//////////////////////////// change this by specific chip ////////////////////////////
-
 #define STC_RAM_SIZE 0x2000   // STC8H8K64U has 8KB xdata
 #define STC_ROM_SIZE 0x10000  // STC8H8K64U has 64KB flash
 
-//////////////////////////// change this by user ////////////////////////////
+//////////////////////////// change this by you want ////////////////////////////
 
 // #define MAIN_Fosc 11520000UL
 // #define MAIN_Fosc 12000000UL
 // #define MAIN_Fosc 22118400UL
 #define MAIN_Fosc 24000000UL
 
-#define DFU_TAG 0x12ABCD34UL  // force DFU mode
-#define DFU_ADDR (STC_RAM_SIZE - sizeof(DFU_TAG))
+#define DFU_TAG 0x12ABCD34UL                       // force DFU mode
+#define DFU_ADDR (STC_RAM_SIZE - sizeof(DFU_TAG))  // at the last 4 bytes of RAM space
 
 #define LDR_VERSION 0x0100  // bootloader version 1.0
 
+//////////////////////////// app info ////////////////////////////
+
+typedef struct {
+    uint16_t addr;
+    uint16_t size;
+    uint32_t crc;
+} app_info_t;
+
 //////////////////////////// FLASH partition ////////////////////////////
 
-#define LDR_SIZE 0x1000                     // bootloader flash space = 4KB
-#define APP_ADDR LDR_SIZE                   // application address
-#define APP_SIZE (STC_ROM_SIZE - LDR_SIZE)  // application size
+// 0x0000 - 0x1000 : bootloader
+// 0x1000 - 0x7000 : application
+// 0x7000 - 0xD000 : backup application
+// 0xD000 - 0x10000 : application eeprom
+
+#define LDR_SIZE 0x1000  // bootloader flash space = 4KB
+#define APP_SIZE 0x6000  // application flash size = 24KB
 
 #define IAP_ADDR_MAX (STC_ROM_SIZE - LDR_SIZE)  // when iap erasing page(by bootloader or application), the `addr` must less than this value
+
 
 #endif /* __COMMON_H__ */
